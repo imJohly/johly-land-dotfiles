f0a2bfea44a5e0c5b794c48412d4bd573496b985
Johnson Ly <johnsonly2002@hotmail.com>
2024-05-27 23:36:30 +1000

    Add hyprlang grammar to tree-sitter

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -15,7 +15,7 @@ require('mason-lspconfig').setup({
         "gopls",
         "jsonls",
         "lua_ls",
-        "pyright",
+        "basedpyright",
         "tsserver",
         "yamlls",
     },
@@ -23,3 +23,4 @@ require('mason-lspconfig').setup({
 		lsp_zero.default_setup,
 	},
 })
+
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,1 @@
 require("johly")
-
-vim.wo.number = true
